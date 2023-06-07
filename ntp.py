a6a2a2125e367b7f9a73dbb81f89fa4f50d35e0a
AinurK <silver_surfer2000@mail.ru>
2023-06-07 12:43:27 +0300

    Update to diplom

--- ntp.py
+++ ntp.py
@@ -10,12 +10,12 @@ def sync_with_ntp(ntp_server):
         response.offset
         delay = response.delay
         current_time = ctime(response.tx_time)
-        #print(f'Good {ntp_server}')
+        print(f'Good {ntp_server}')
         #print(f'Current time {datetime.fromtimestamp(response.tx_time, timezone.utc)}')
         print(f'Delay {delay}')
     except Exception as e:
         print(f'Bad {str(e)}')
     return float(delay)
 
-ntp_server = 'ntp1.ntp-servers.net'
+ntp_server = 'ntp7.ntp-servers.net'
 sync_with_ntp(ntp_server)
--- text.py
+++ text.py
@@ -3,51 +3,68 @@ from pyqtgraph import PlotWidget
 import pyqtgraph as pg
 import sys
 from ntp import sync_with_ntp
+import subprocess
+
+# ntp1.ntp-servers.net
 
 class MainWindow(QtWidgets.QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        #Load the UI Page
         uic.loadUi('qt_kuddus.ui', self)
         self.graphWidget.setBackground('w')
         
         styles = {'color':'r', 'font-size':'15px'}
         self.graphWidget.setLabel('left', 'Delay (s)', **styles)
-        self.graphWidget.setLabel('bottom', 'Seconds (s)', **styles) #  
-        self.graphWidget.showGrid(x=True, y=True) #  сетка
+        self.graphWidget.setLabel('bottom', 'Seconds (s)', **styles)
+        self.graphWidget.showGrid(x=True, y=True)
+        self.graphWidget.setEnabled(False)
+
+        self.button_connect.clicked.connect(self.connect1)
+        self.button_sync.clicked.connect(self.sync1)
 
 
-        self.ui.button_connect.clicked.connect(lambda: print(self.listEdit_server.text()))
+    def sync1(self):
+        commands = []
+        linux = [f'chrony '] # комманда для синхронизации 
+        windows = [f'w32tm ' ]
+        if self.comboBox.currentText() == 'Linux':
+            commands = linux
+            print('linux')
+        elif self.comboBox.currentText() == 'Windows':
+            commands = windows
+            print('windows')
+
+        #for i in commands:
+            #subprocess.call(i)
 
 
-        #  self.graphWidget.clear()
+    def connect1(self):
+        if self.button_connect.text() == 'Подключится':
+            self.button_connect.setText('Отключится')
 
-        self.y = [float(sync_with_ntp('ntp1.ntp-servers.net')) for i in range(10)] 
-        self.x = [i for i in range(10)]
+            self.y = [0 for i in range(10)] 
+            self.x = [i for i in range(10)]
 
+            my_pen = pg.mkPen(color=(255, 0, 0))
+            self.data_line= self.graphWidget.plot(self.x, self.y, pen=my_pen)
 
-        my_pen = pg.mkPen(color=(255, 0, 0))
-        self.data_line= self.graphWidget.plot(self.x, self.y, pen=my_pen)
-
-        self.timer = QtCore.QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.update_plot_data)
-        self.timer.start()
+            self.timer = QtCore.QTimer()
+            self.timer.setInterval(1000)
+            self.timer.timeout.connect(self.update_plot_data)
+            self.timer.start()
+        else:
+            self.timer.stop()
+            self.graphWidget.clear()
+            self.button_connect.setText('Подключится')
 
 
     def update_plot_data(self):
-        #  self.x.append(self.x[-1] + 1)
-
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
-
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(sync_with_ntp('ntp1.ntp-servers.net'))  # Add a new random value.
-
-        self.data_line.setData(self.x, self.y)  # Update the data.
-
+        self.x = self.x[1:]
+        self.x.append(self.x[-1] + 1)
+        self.y = self.y[1:]
+        self.y.append(sync_with_ntp(self.lineEdit.text()))
+        self.data_line.setData(self.x, self.y)
 
 
 def main():
